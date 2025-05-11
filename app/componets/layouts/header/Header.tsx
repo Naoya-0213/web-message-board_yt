56c077ea90b0d8646540685d58c453b57518fd52
Naoya Noguchi <naoya.work0213@gmail.com>
2025-05-11 14:11:51 +0900

    Header変更

--- app/componets/layouts/header/Header.tsx
+++ app/componets/layouts/header/Header.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Header() {
-  return <h1 className="">Header</h1>;
+  return (
+    <h1 className="divide-y border-gray-200 dark:border-gray-800 border-b">
+      Header
+    </h1>
+  );
 }
 
 export default Header;
