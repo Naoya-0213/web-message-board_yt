691aa958217aaeae6a58ac9101ce1e23faebf2da
Naoya Noguchi <naoya.work0213@gmail.com>
2025-05-11 15:07:29 +0900

    shadcn導入 button / card

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start"></main>
+    <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+
+
+      
+    </main>
   );
 }
