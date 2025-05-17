f38b814c74884242f885f65d3a699dc0583d9611
Naoya Noguchi <naoya.work0213@gmail.com>
2025-05-17 13:52:08 +0900

    create/FormField追加

---
+++ app/bbs-posts/create/page.tsx
@@ -0,0 +1,101 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Form } from "@/components/ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import React from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+// React-hook-form
+const formSchema = z.object({
+  username: z
+    .string()
+    .min(2, { message: "ユーザー名は2文字以上で入力してください。" }),
+  title: z
+    .string()
+    .min(2, { message: "タイトル名は2文字以上で入力してください。" }),
+  content: z
+    .string()
+    .min(10, { message: "本文は10文字以上で入力してください。" })
+    .max(140, { message: "本文は140文字以内で入力してください。" }),
+});
+
+const CreateBBCPage = () => {
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      title: "",
+      content: "",
+    },
+  });
+
+  // onSubmit指定
+  async function onSubmit() {}
+
+  return (
+    <div>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          {/* ユーザー名 */}
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>ユーザー名</FormLabel>
+                <FormControl>
+                  <Input placeholder="ユーザー名を入力" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          {/* タイトル */}
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>タイトル</FormLabel>
+                <FormControl>
+                  <Input placeholder="タイトルを入力" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          {/* 本文 */}
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>本文</FormLabel>
+                <FormControl>
+                  <Input placeholder="本文を入力" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    </div>
+  );
+};
+
+export default CreateBBCPage;
