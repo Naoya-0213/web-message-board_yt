bb262dfcb127458691222bc672eab34df1cbf6b8
Naoya Noguchi <naoya.work0213@gmail.com>
2025-05-17 13:27:57 +0900

    s create作成開始

--- app/bbs-posts/[bbsId]/page.tsx
+++ app/bbs-posts/[bbsId]/page.tsx
@@ -1,4 +1,5 @@
 import type { BBSData } from "@/app/types/types";
+import Link from "next/link";
 import React from "react";
 
 async function getDetailBBSData(id: number) {
@@ -13,7 +14,24 @@ async function getDetailBBSData(id: number) {
 
 const BBSDetailPage = async ({ params }: { params: { bbsId: number } }) => {
   const bbsDetailData = await getDetailBBSData(Number(params.bbsId));
-  return <div>BBSDetailPage</div>;
+  const { title, content, username } = bbsDetailData;
+
+  return (
+    <div className="mx-auto max-w-4xl p-4">
+      <div className="md-8">
+        <h1 className="text-2xl font-bold">{title}</h1>
+        <p className="text-gray-700">{username}</p>
+      </div>
+
+      <div className="mb-8">
+        <p className="text-gray-900">{content}</p>
+      </div>
+
+      <Link href={"/"} className="bg-blue-500 text-white font-bold py-2 px-4 rounded-md">
+        戻る
+      </Link>
+    </div>
+  );
 };
 
 export default BBSDetailPage;
